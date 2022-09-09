7f737c98e101359ceedfad60a560974de8ed88c1
sh1hsh1nk <shashank.motepalli@mail.utoronto.ca>
2022-09-10 02:05:07 +0530

    dec rate

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -178,7 +178,7 @@ def remote(ctx, debug=False):
         'nodes': [4],
         'workers': 1,
         'collocate': True,
-        'rate': [1_000_000_000_000],
+        'rate': [100_000_000_000],
         'tx_size': 32,
         'duration': 300,
         'runs': 2,
